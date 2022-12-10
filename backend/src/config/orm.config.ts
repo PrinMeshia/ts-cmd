8512f302f09ff29f55686ddddb872b22fbdc083b
Jonathan vallée <j.vallee@programisto.fr>
2022-12-10 18:41:30 +0100

    remove element for typeorm0.3

--- backend/src/config/orm.config.ts
+++ backend/src/config/orm.config.ts
@@ -9,9 +9,9 @@ const ormConfig: ConnectionOptions = {
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
-  cli: {
-    migrationsDir: 'src/migrations',
-  },
+  // cli: {
+  //   migrationsDir: 'src/migrations',
+  // },
 };
 
 export default ormConfig;
